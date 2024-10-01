50c803dc976c8a9b94e69dcccdacaadc87487a24
doomiinguees <2220897@my.ipleiria.pt>
2024-10-01 21:12:05 +0100

    Updated Database

--- database/gofuel.sql
+++ database/gofuel.sql
@@ -1,97 +1,283 @@
--- Active: 1727279290726@@127.0.0.1@3306
-# CREATE DATABASE gofuel DEFAULT CHARACTER SET = 'utf8mb4';
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gofuel
+-- ------------------------------------------------------
+-- Server version	8.3.0
 
-USE gofuel;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Criação da tabela user_info
-CREATE TABLE user_info (
-    id INTEGER PRIMARY KEY,
-    user_id INTEGER, -- Referência para o id da tabela default do Yii2 Framework
-    nif INTEGER,
-    role ENUM(
-        'Admin',
-        'Manager',
-        'In Charge',
-        'Employee'
-    ),
-    email VARCHAR(255),
-    name VARCHAR(255),
-    address VARCHAR(255),
-    postal_code VARCHAR(20),
-    CONSTRAINT fk_user_info_user_id FOREIGN KEY (user_id) REFERENCES default_user (id) -- Referência à tabela padrão de usuários do Yii2
-);
+--
+-- Table structure for table `categories`
+--
 
--- Criação da tabela items
-CREATE TABLE items (
-    id INTEGER PRIMARY KEY,
-    description VARCHAR(255),
-    price DOUBLE,
-    subcategory_id INTEGER,
-    CONSTRAINT fk_items_subcategory_id FOREIGN KEY (subcategory_id) REFERENCES subcategories (id)
-);
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Criação da tabela categories
-CREATE TABLE categories (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255)
-);
+--
+-- Dumping data for table `categories`
+--
 
--- Criação da tabela subcategories
-CREATE TABLE subcategories (
-    id INTEGER PRIMARY KEY,
-    description VARCHAR(255),
-    category_id INTEGER,
-    CONSTRAINT fk_subcategories_category_id FOREIGN KEY (category_id) REFERENCES categories (id)
-);
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Criação da tabela item_stocks
-CREATE TABLE item_stocks (
-    id INTEGER PRIMARY KEY,
-    item_id INTEGER,
-    restock_qty INTEGER,
-    CONSTRAINT fk_item_stocks_item_id FOREIGN KEY (item_id) REFERENCES items (id)
-);
+--
+-- Table structure for table `invoice_lines`
+--
 
--- Criação da tabela stations
-CREATE TABLE stations (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    address VARCHAR(255),
-    postal_code VARCHAR(20)
-);
+DROP TABLE IF EXISTS `invoice_lines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `invoice_lines` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `qty` int NOT NULL,
+  `total` double NOT NULL,
+  `invoice_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_invoice_lines_invoice_id` (`invoice_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Criação da tabela pumps
-CREATE TABLE pumps (
-    id INTEGER PRIMARY KEY,
-    station_id INTEGER,
-    CONSTRAINT fk_pumps_station_id FOREIGN KEY (station_id) REFERENCES stations (id)
-);
+--
+-- Dumping data for table `invoice_lines`
+--
 
--- Criação da tabela invoice_lines
-CREATE TABLE invoice_lines (
-    id INTEGER PRIMARY KEY,
-    unit_price DOUBLE,
-    qty INTEGER,
-    total DOUBLE,
-    invoice_id INTEGER,
-    CONSTRAINT fk_invoice_lines_invoice_id FOREIGN KEY (invoice_id) REFERENCES invoices (id)
-);
+LOCK TABLES `invoice_lines` WRITE;
+/*!40000 ALTER TABLE `invoice_lines` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoice_lines` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Criação da tabela invoices
-CREATE TABLE invoices (
-    id INTEGER PRIMARY KEY,
-    client_id INTEGER,
-    station_id INTEGER,
-    invoice_date DATE,
-    total DOUBLE,
-    state_id INTEGER,
-    CONSTRAINT fk_invoices_client_id FOREIGN KEY (client_id) REFERENCES user_info (id),
-    CONSTRAINT fk_invoices_station_id FOREIGN KEY (station_id) REFERENCES stations (id),
-    CONSTRAINT fk_invoices_state_id FOREIGN KEY (state_id) REFERENCES invoice_states (id)
-);
+--
+-- Table structure for table `invoice_states`
+--
 
--- Criação da tabela invoice_states
-CREATE TABLE invoice_states (
-    id INTEGER PRIMARY KEY,
-    description VARCHAR(255) -- Descrição do estado (ex: 'Cart', 'Paid', 'Finish')
-);
+DROP TABLE IF EXISTS `invoice_states`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `invoice_states` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoice_states`
+--
+
+LOCK TABLES `invoice_states` WRITE;
+/*!40000 ALTER TABLE `invoice_states` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoice_states` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invoices`
+--
+
+DROP TABLE IF EXISTS `invoices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `invoices` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `client_id` int NOT NULL,
+  `station_id` int NOT NULL,
+  `invoice_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `total` double NOT NULL,
+  `state_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_invoices_client_id` (`client_id`),
+  KEY `fk_invoices_station_id` (`station_id`),
+  KEY `fk_invoices_state_id` (`state_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoices`
+--
+
+LOCK TABLES `invoices` WRITE;
+/*!40000 ALTER TABLE `invoices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item_stocks`
+--
+
+DROP TABLE IF EXISTS `item_stocks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `item_stocks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `item_id` int NOT NULL,
+  `restock_qty` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_item_stocks_item_id` (`item_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item_stocks`
+--
+
+LOCK TABLES `item_stocks` WRITE;
+/*!40000 ALTER TABLE `item_stocks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `item_stocks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `items` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) NOT NULL,
+  `price` double NOT NULL,
+  `subcategory_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_items_subcategory_id` (`subcategory_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pumps`
+--
+
+DROP TABLE IF EXISTS `pumps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pumps` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `station_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_pumps_station_id` (`station_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pumps`
+--
+
+LOCK TABLES `pumps` WRITE;
+/*!40000 ALTER TABLE `pumps` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pumps` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `stations`
+--
+
+DROP TABLE IF EXISTS `stations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `stations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `postal_code` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stations`
+--
+
+LOCK TABLES `stations` WRITE;
+/*!40000 ALTER TABLE `stations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `stations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `subcategories`
+--
+
+DROP TABLE IF EXISTS `subcategories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subcategories` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) NOT NULL,
+  `category_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_subcategories_category_id` (`category_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subcategories`
+--
+
+LOCK TABLES `subcategories` WRITE;
+/*!40000 ALTER TABLE `subcategories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `subcategories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_info`
+--
+
+DROP TABLE IF EXISTS `user_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_info` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `nif` int NOT NULL,
+  `role` enum('Admin','Manager','In Charge','Employee') NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `postal_code` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_info_user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_info`
+--
+
+LOCK TABLES `user_info` WRITE;
+/*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-10-01 21:10:02
