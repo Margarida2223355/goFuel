5436e066082ecd03e2dc9a90df52f3ff86f42057
Margarida Camacho <2223355@my.ipleiria.pt>
2024-10-03 00:24:40 +0100

    SplashScreen with animation.

--- Mobile/app/build.gradle.kts
+++ Mobile/app/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
 android {
     namespace = "com.example.gofuel"
     compileSdk = 34
+    viewBinding {
+        enable = true
+    }
 
     defaultConfig {
         applicationId = "com.example.gofuel"
