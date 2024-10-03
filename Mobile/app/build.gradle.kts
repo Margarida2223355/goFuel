7ebe83344c9fdc5877c1b6fb8b53f265a6f383b3
Margarida Camacho <2223355@my.ipleiria.pt>
2024-10-03 14:47:56 +0100

    ResultWrapper - result or error from API call

--- Mobile/app/build.gradle.kts
+++ Mobile/app/build.gradle.kts
@@ -43,4 +43,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Retrofit to API calls
+    implementation(libs.retrofit2)
+    implementation(libs.retrofit2Conversor)
 }
