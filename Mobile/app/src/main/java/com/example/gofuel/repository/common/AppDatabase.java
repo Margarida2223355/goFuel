98d09a5fc52ce01623fe9724a9b952de7dfdb6e6
Margarida Camacho <2223355@my.ipleiria.pt>
2024-10-05 21:43:15 +0100

    Create local DB.

--- Mobile/app/src/main/java/com/example/gofuel/repository/common/AppDatabase.java
+++ Mobile/app/src/main/java/com/example/gofuel/repository/common/AppDatabase.java
@@ -6,7 +6,6 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import com.example.gofuel.model.Station;
-import com.example.gofuel.repository.station.local.StationDB;
 
 @Database(
         entities = {Station.class},
@@ -14,7 +13,6 @@ import com.example.gofuel.repository.station.local.StationDB;
 )
 public abstract class AppDatabase extends RoomDatabase {
     private static volatile AppDatabase INSTANCE;
-    public abstract StationDB stationDB();
 
     public static AppDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
