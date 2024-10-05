cb9b8b61de3e71d12103612a8eda7171020f3468
Margarida Camacho <2223355@my.ipleiria.pt>
2024-10-05 13:13:51 +0100

    Add data source types interfaces (remote and local).

--- Mobile/app/src/main/java/com/example/gofuel/repository/station/IStationDataSource.java
+++ Mobile/app/src/main/java/com/example/gofuel/repository/station/IStationDataSource.java
@@ -3,14 +3,12 @@ package com.example.gofuel.repository.station;
 import com.example.gofuel.model.Station;
 import com.example.gofuel.repository.common.ResultWrapper;
 
-import java.util.List;
-
 public interface IStationDataSource {
     interface Common {}
 
     // Remote data source
     interface Remote {
-        ResultWrapper<List<Station>> getStations();
+        ResultWrapper<Station> getStation();
     }
 
     // Local data source
