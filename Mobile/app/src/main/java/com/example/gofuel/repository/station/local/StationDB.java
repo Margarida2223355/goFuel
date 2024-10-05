c05c00b31dfe9dd23acca23ad976497caaf08db5
Margarida Camacho <2223355@my.ipleiria.pt>
2024-10-05 21:40:54 +0100

    Add interface for StationDB (use room library).

--- Mobile/app/src/main/java/com/example/gofuel/repository/station/local/StationDB.java
+++ Mobile/app/src/main/java/com/example/gofuel/repository/station/local/StationDB.java
@@ -4,7 +4,7 @@ import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 
-import com.example.gofuel.model.station.Station;
+import com.example.gofuel.model.Station;
 import java.util.List;
 
 @Dao
