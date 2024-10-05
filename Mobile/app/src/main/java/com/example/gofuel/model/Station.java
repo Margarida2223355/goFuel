680f5cb77eb491dbb37effabe16d81845c36b642
Margarida Camacho <2223355@my.ipleiria.pt>
2024-10-05 21:40:08 +0100

    Change Station model to use local DB.

--- Mobile/app/src/main/java/com/example/gofuel/model/Station.java
+++ Mobile/app/src/main/java/com/example/gofuel/model/Station.java
@@ -1,7 +1,12 @@
 package com.example.gofuel.model;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "stations")
 public class Station {
     //region Properties
+    @PrimaryKey
     private int id;
     private String name;
     private String address;
