09775f1b523cff771375aefee0349b9065e6ab25
Margarida Camacho <2223355@my.ipleiria.pt>
2024-10-05 16:59:43 +0100

    Change results from API call to list of stations.

--- Mobile/app/src/main/java/com/example/gofuel/repository/station/IStationDataSource.java
+++ Mobile/app/src/main/java/com/example/gofuel/repository/station/IStationDataSource.java
@@ -3,12 +3,14 @@ package com.example.gofuel.repository.station;
 import com.example.gofuel.model.Station;
 import com.example.gofuel.repository.common.ResultWrapper;
 
+import java.util.List;
+
 public interface IStationDataSource {
     interface Common {}
 
     // Remote data source
     interface Remote {
-        ResultWrapper<Station> getStation();
+        ResultWrapper<List<Station>> getStations();
     }
 
     // Local data source
