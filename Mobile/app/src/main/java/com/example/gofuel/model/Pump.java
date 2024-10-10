32f29fe18fdae3e1ebeabd9845647e476d89e412
Margarida Camacho <2223355@my.ipleiria.pt>
2024-10-10 21:19:02 +0100

    Create pump model.

---
+++ Mobile/app/src/main/java/com/example/gofuel/model/Pump.java
@@ -0,0 +1,30 @@
+package com.example.gofuel.model;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.example.gofuel.model.station.Station;
+
+@Entity(tableName = "pumps")
+public class Pump {
+    @PrimaryKey
+    private final int id;
+    private Station station;
+
+    public Pump(int id, Station station) {
+        this.id = id;
+        this.station = station;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Station getStation() {
+        return station;
+    }
+
+    public void setStation(Station station) {
+        this.station = station;
+    }
+}
