38322762c07000a198eeb641b369466b39a80f18
doomiinguees <2220897@my.ipleiria.pt>
2024-10-12 17:29:23 +0100

    Migração authmanager

--- WebModule/console/migrations/m241008_222306_init_rbac_roles.php
+++ WebModule/console/migrations/m241008_222306_init_rbac_roles.php
@@ -25,38 +25,14 @@ class m241008_222306_init_rbac_roles extends Migration
         $auth->add($employee);
         $auth->add($client);
 
-        // Se quiser adicionar permissões específicas, faça isso aqui.
-        // Exemplo de permissão:
-        // $viewInvoices = $auth->createPermission('viewInvoices');
-        // $auth->add($viewInvoices);
-        // $auth->addChild($manager, $viewInvoices);
 
-        // Atribuir uma role ao usuário (exemplo: Admin ao usuário de ID 1)
-        // Você pode modificar ou adicionar mais atribuições conforme necessário
-        $auth->assign($admin, 1); // Admin atribuído ao usuário com ID 1
+        $auth->assign($admin, 1);
     }
 
     public function safeDown()
     {
         $auth = Yii::$app->authManager;
-
-        // Remover todas as roles
         $auth->removeAll();
     }
 
-
-    /*
-    // Use up()/down() to run migration code without a transaction.
-    public function up()
-    {
-
-    }
-
-    public function down()
-    {
-        echo "m241008_222306_init_rbac_roles cannot be reverted.\n";
-
-        return false;
-    }
-    */
 }
