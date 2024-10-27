516623070b0144eac09c74c56a5b14478662e650
Margarida Camacho <2223355@my.ipleiria.pt>
2024-10-27 01:30:16 +0100

    Empty State.

--- Mobile/app/src/main/java/com/example/gofuel/view/Fragments/StationFragment.java
+++ Mobile/app/src/main/java/com/example/gofuel/view/Fragments/StationFragment.java
@@ -39,13 +39,20 @@ public class StationFragment extends Fragment {
         viewModel.getState().observe(getViewLifecycleOwner(), state -> {
             if (state instanceof State.Loading) {
                 binding.stationList.setVisibility(View.GONE);
+                binding.emptyState.setVisibility(View.GONE);
                 binding.loading.setVisibility(View.VISIBLE);
             }
             else if (state instanceof State.StationList) {
                 binding.loading.setVisibility(View.GONE);
+                binding.emptyState.setVisibility(View.GONE);
                 binding.stationList.setVisibility(View.VISIBLE);
                 ArrayList<Station> stations = new ArrayList<>(((State.StationList) state).getStations());
                 binding.stationList.setAdapter(new StationAdapter(getContext(), stations));
+            }
+            else if (state instanceof State.EmptyState) {
+                binding.stationList.setVisibility(View.GONE);
+                binding.loading.setVisibility(View.GONE);
+                binding.emptyState.setVisibility(View.VISIBLE);
             }
         });
 
