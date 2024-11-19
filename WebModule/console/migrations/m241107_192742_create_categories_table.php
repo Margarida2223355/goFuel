89ffbf7af838eb32122e4eddba021adafc6ab3ac
doomiinguees <2220897@my.ipleiria.pt>
2024-11-19 22:01:51 +0000

    teste data

--- WebModule/console/migrations/m241107_192742_create_categories_table.php
+++ WebModule/console/migrations/m241107_192742_create_categories_table.php
@@ -13,6 +13,12 @@ class m241107_192742_create_categories_table extends Migration
             'id' => $this->primaryKey(),
             'name' => $this->string()->notNull(),
         ]);
+
+        $this->batchInsert('categories', ['id', 'name'], [
+            [1, 'Category 1'],
+            [2, 'Category 2'],
+            [3, 'Category 3'],
+        ]);
     }
 
     public function safeDown()
