2b2e1d2ac62e27951aa8794914ec26d8193c6ec5
Margarida Camacho <2223355@my.ipleiria.pt>
2024-11-23 14:45:42 +0000

    IP to server IP

--- Mobile/app/src/main/java/com/example/gofuel/util/Constants.java
+++ Mobile/app/src/main/java/com/example/gofuel/util/Constants.java
@@ -2,8 +2,7 @@ package com.example.gofuel.util;
 
 public class Constants {
     //region BASE_URL
-    //public static final String BASE_URL = "http://10.0.2.2:8888/backend/web/api/"; // School
-    public static final String BASE_URL = "http://192.168.1.87:8888/backend/web/api/"; // Home
+    public static final String BASE_URL = "http://54.242.246.148:8080/api/"; // Home
     //endregion
 
     //region HTTP headers
