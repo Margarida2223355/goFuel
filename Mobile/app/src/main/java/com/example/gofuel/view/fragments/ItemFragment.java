b02ea27b00f33214def6fcdb3a82886ad53a0ad0
Margarida Camacho <2223355@my.ipleiria.pt>
2024-11-26 21:27:32 +0000

    Item fragment layout

---
+++ Mobile/app/src/main/java/com/example/gofuel/view/fragments/ItemFragment.java
@@ -0,0 +1,30 @@
+package com.example.gofuel.view.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.gofuel.R;
+import com.example.gofuel.databinding.FragmentItemBinding;
+
+public class ItemFragment extends Fragment {
+    private FragmentItemBinding binding;
+
+    public ItemFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentItemBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        
+        return view;
+    }
+}
