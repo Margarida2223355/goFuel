7732bc4ba21654068006fdd5772afb0958cf07be
Margarida Camacho <2223355@my.ipleiria.pt>
2024-12-06 15:10:19 +0000

    invoice lines api call

--- WebModule/backend/modules/api/controllers/InvoicelineController.php
+++ WebModule/backend/modules/api/controllers/InvoicelineController.php
@@ -6,5 +6,25 @@
 
     class InvoicelineController extends ActiveController {
         public $modelClass = Invoiceline::class;
+
+        // Disable default action "index"
+        public function actions()
+        {
+            $actions = parent::actions();
+            unset($actions['index']);
+            return $actions;
+        }
+
+        public function actionIndex() {
+            $invoiceID = \Yii::$app -> request -> getHeaders() -> get('X-INVOICE-ID');
+
+            if (!$invoiceID) {
+                throw new UnauthorizedHttpException('No station ID provided');
+            }
+
+            return Invoiceline::find()
+                ->where(['invoice_id' => $invoiceID])
+                ->all();
+        }
     }
 ?>
