c5d5b3bdcf15afda2391298b689f800e6d01df36
Margarida Camacho <2223355@my.ipleiria.pt>
2025-01-28 22:56:47 +0000

    Add images to item and station models

--- Mobile/app/src/main/java/com/example/gofuel/model/item/Item.java
+++ Mobile/app/src/main/java/com/example/gofuel/model/item/Item.java
@@ -1,20 +1,27 @@
 package com.example.gofuel.model.item;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 import com.example.gofuel.model.subcategory.Subcategory;
+import com.example.gofuel.util.Util;
+
+import java.util.Base64;
 
 @Entity(tableName = "items")
 public class Item {
     @PrimaryKey private final int id;
-    private String description;
+    private String description, image;
     private Subcategory subcategory;
 
-    public Item(int id, String description, Subcategory subcategory) {
+    public Item(int id, String description, Subcategory subcategory, String image) {
         this.id = id;
         this.description = description;
         this.subcategory = subcategory;
+        this.image = image;
     }
 
     public int getId() {
@@ -29,11 +36,19 @@ public class Item {
         return subcategory;
     }
 
+    public String getImage() {
+        return image;
+    }
+
     public void setDescription(String description) {
         this.description = description;
     }
 
     public void setSubcategory(Subcategory subcategory) {
         this.subcategory = subcategory;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 }
