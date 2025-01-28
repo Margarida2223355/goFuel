dd5c8d3638b56c67159b82e371324db90428a03e
Margarida Camacho <2223355@my.ipleiria.pt>
2025-01-28 23:03:22 +0000

    Update version

--- Mobile/app/build.gradle.kts
+++ Mobile/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.example.gofuel"
         minSdk = 28
         targetSdk = 34
-        versionCode = 12
+        versionCode = 13
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
