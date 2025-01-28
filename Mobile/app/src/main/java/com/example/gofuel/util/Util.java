9a02248517fdb32d74cf1603ce51bc111669a9ca
Margarida Camacho <2223355@my.ipleiria.pt>
2025-01-28 22:57:08 +0000

    Create decoder image base64 method

--- Mobile/app/src/main/java/com/example/gofuel/util/Util.java
+++ Mobile/app/src/main/java/com/example/gofuel/util/Util.java
@@ -1,7 +1,14 @@
 package com.example.gofuel.util;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.example.gofuel.MyApplication;
+import com.example.gofuel.R;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Base64;
 import java.util.Locale;
 
 public class Util {
@@ -14,6 +21,17 @@ public class Util {
         }
         catch (ParseException e) {
             return "Data Inválida: " + e.getMessage();
+        }
+    }
+
+    public static Bitmap convertToImage(String image) {
+        try {
+            byte[] decodedBytes = Base64.getDecoder().decode(image);
+            return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+        }
+        catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return BitmapFactory.decodeResource(MyApplication.getAppContext().getResources(), R.drawable.item);
         }
     }
 }
