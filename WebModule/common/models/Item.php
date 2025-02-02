98c8c52a1f41a3ef5a847833664a07174317c9f8
doomiinguees <2220897@my.ipleiria.pt>
2025-02-02 02:12:00 +0000

    delete dump and die

--- WebModule/common/models/Item.php
+++ WebModule/common/models/Item.php
@@ -117,7 +117,7 @@ class Item extends \yii\db\ActiveRecord
 
             $fileName = $this->imageFile->name;
             $filePath = $uploadPath . $fileName;
-dd($filePath);
+
             if ($this->imageFile->saveAs($filePath)) {
                 $this->image = $fileName;
                 $this->save();
